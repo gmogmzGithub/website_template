c9bdcf5d6561bd79c67367154a4a2b7318a994c0
El Men <guillermogomezmora@gmail.com>
2024-08-07 21:45:09 -0600

    Initial commit

--- appy-next/next.config.js
+++ appy-next/next.config.js
@@ -1,13 +1,28 @@
-const { withContentlayer } = require('next-contentlayer');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
+
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/nextjs-github-pages",
+
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
     images: {
-        unoptimized: true
+        unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/website_template' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/website_template' : '',
 };
 
-module.exports = withContentlayer(nextConfig);
+export default nextConfig;
